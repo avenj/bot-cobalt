c01e1d94c67de0cadfb87101edc751fae2783d0f
avenj <avenj@eris.oppresses.us>
2011-12-15 19:11:28 +0000

    Utils.pm: timestr_to_secs from cobalt0.11

--- lib/Cobalt/Utils.pm
+++ lib/Cobalt/Utils.pm
@@ -8,9 +8,25 @@ require Exporter;
 our @ISA = qw(Exporter);
 
 our @EXPORT = qw/
+  timestr_to_secs
 
 /;
 
+sub timestr_to_secs {
+  ## turn something like 2h3m30s into seconds
+  my $timestr = shift;
+  my($hrs,$mins,$secs,$total);
+  if ($timestr =~ m/(\d+)h/)
+    { $hrs = $1; }
+  if ($timestr =~ m/(\d+)m/)
+    { $mins = $1; }
+  if ($timestr =~ m/(\d+)s/)
+    { $secs = $1; }
+  $total = $secs;
+  $total += (int $mins * 60) if $mins;
+  $total += (int $hrs * 3600) if $hrs;
+  return int($total)
+}
 
 
 1;
