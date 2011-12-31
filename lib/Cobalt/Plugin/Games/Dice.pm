e0bca03772101821934b4956f0ec1f141ef51bad
avenj <avenj@cobaltirc.org>
2011-12-31 04:23:02 -0500

    Plugin::Games::

---
+++ lib/Cobalt/Plugin/Games/Dice.pm
@@ -0,0 +1,17 @@
+package Cobalt::Plugin::Games::Dice;
+
+our $VERSION = '0.01';
+
+sub new { bless( {}, shift ) }
+
+## Games plugins are called thusly:
+## $obj->execute($msg_hash)
+sub execute {
+
+}
+
+sub _roll {
+
+}
+
+1;
