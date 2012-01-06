91ec12fcbc08d49d662ff46f20163a00c4c0c5ee
avenj <avenj@cobaltirc.org>
2012-01-06 18:10:15 -0500

    Cobalt.pm; _auth_ accessors

--- lib/Cobalt.pm
+++ lib/Cobalt.pm
@@ -399,10 +399,77 @@ sub timer_del_pkg {
 ## These are just easy ways to get at the hash.
 
 sub auth_level {
+  ## retrieve an auth level for $nickname in $context
+  ## unidentified users get access level 0 by default
+  ## FIXME: configurable default access level
   my ($self, $context, $nickname) = @_;
 
+  return 0 unless exists $self->State->{Auth}->{$context};
+  my $context_rec = $self->State->{Auth}->{$context};
+
+  return 0 unless exists $context_rec->{$nickname};
+  my $level = $context_rec->{$nickname}->{Level} // 0;
+
+  return $level
 }
 
+sub auth_username {
+  ## retrieve an auth username by context -> IRC nick
+  ## retval is boolean untrue if user can't be found
+  my ($self, $context, $nickname) = @_;
+
+  return unless exists $self->State->{Auth}->{$context};
+  my $context_rec = $self->State->{Auth}->{$context};
+
+  return unless exists $context_rec->{$nickname};
+  my $username = $context_rec->{$nickname}->{Username};
+
+  return $username ? $username : ();
+}
+
+sub auth_flags {
+  ## retrieve auth flags by context -> IRC nick
+  ##
+  ## untrue if record can't be found
+  ##
+  ## otherwise you get a reference to the Flags hash in Auth
+  ##
+  ## this means you can modify flags:
+  ##  my $flags = $core->auth_flags($context, $nick);
+  ##  $flags->{SUPERUSER} = 1;
+
+  my ($self, $context, $nickname) = @_;
+
+  return unless exists $self->State->{Auth}->{$context};
+  my $context_rec = $self->State->{Auth}->{$context};
+
+  return unless exists $context_rec->{$nickname};
+  return unless ref $context_rec->{$nickname}->{Flags} eq 'HASH';
+  return $context_rec->{$nickname}->{Flags};
+}
+
+sub auth_pkg {
+  ## retrieve the __PACKAGE__ that provided this user's auth
+  ## (in other words, the plugin that created the hash)
+  my ($self, $context, $nickname) = @_;
+
+  return unless exists $self->State->{Auth}->{$context};
+  my $context_rec = $self->State->{Auth}->{$context};
+
+  return unless exists $context_rec->{$nickname};
+  my $pkg = $context_rec->{$nickname}->{Package};
+
+  return $pkg ? $pkg : ();
+}
+
+
+sub get_irc_obj {
+  ## retrieve our POE::Component::IRC obj for $context
+  my ($self, $context) = @_;
+  return unless exists $self->Servers->{$context};
+  my $irc = $self->Servers->{$context}->{Object} // return;
+  return ref $irc ? $irc : ();
+}
 
 
 
