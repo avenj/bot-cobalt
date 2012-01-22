899e504c2c22f314581115efda0cabc30a510cb2
avenj <avenj@cobaltirc.org>
2012-01-22 01:56:05 -0500

    Plugin::Rehash framework

---
+++ lib/Cobalt/Plugin/Rehash.pm
@@ -0,0 +1,50 @@
+package Cobalt::Plugin::Rehash;
+our $VERSION = '0.10';
+
+## HANDLES AND EATS:
+##  !rehash
+##
+##  Rehash channels.conf & plugins.conf
+##
+##  Does NOT rehash plugin confs
+##  Plugins often do some initialization after a conf load
+##  Reload them using PluginMgr's !reload function instead.
+##
+## Also doesn't make very many guarantees regarding consequences ...
+
+use 5.12.1;
+use strict;
+use warnings;
+
+use Cobalt::Conf;
+
+sub new { bless {}, shift }
+
+sub Cobalt_register {
+  my ($self, $core) = splice @_, 0, 2;
+  $self->{core} = $core;
+  $core->plugin_register( $self, 'SERVER',
+    [ 'public_cmd_rehash' ]
+  );
+  $core->log->info("Registered, commands: !rehash");
+  return PLUGIN_EAT_NONE
+}
+
+sub Cobalt_unregister {
+  my ($self, $core) = splice @_, 0, 2;
+  $self->{core} = $core;
+  $core->log->info("Unregistered");
+  return PLUGIN_EAT_NONE
+}
+
+sub Bot_public_cmd_rehash {
+  my ($self, $core) = splice @_, 0, 2;
+
+  ## FIXME
+  ##  use Cobalt::Conf to grab and reload _core_confs for channels/plugin
+  ##  replace $core->cfg->{channels}/{plugins}
+
+  return PLUGIN_EAT_ALL
+}
+
+1;
