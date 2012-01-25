8f658b0670331b5b20f0a56d4eb5523b6535f143
avenj <avenj@cobaltirc.org>
2012-01-25 01:44:16 -0500

    Plugin::Info3; _info_format

--- lib/Cobalt/Plugin/Info3.pm
+++ lib/Cobalt/Plugin/Info3.pm
@@ -74,6 +74,9 @@ sub Bot_public_msg {
   my ($self, $core) = splice @_, 0, 2;
   my $context = ${$_[0]};
   my $msg = ${$_[1]};
+  
+  ## if this is a !cmd, discard it:
+  return PLUGIN_EAT_NONE if $msg->{cmdprefix};
 
 
   return PLUGIN_EAT_NONE
@@ -86,8 +89,8 @@ sub Bot_info3_relay_string {
   my $nick    = ${$_[2]};
   my $string  = ${$_[3]};
 
+  ## received from RDB when handing off ~rdb topics
   ## FIXME _info_format and send
-  ##
 
   return PLUGIN_EAT_NONE
 }
@@ -120,12 +123,61 @@ sub _info_search {
 }
 
 sub _info_match {
+  ## FIXME get entire msg .. ?
   ## see if text matches
   ## if topic contains a rdb, send rdb_triggered to talk to RDB.pm
 }
 
 sub _info_format {
+  my ($self, $context, $nick, $channel, $str) = @_;
   ## variable replacement for responses
+  ## some of these need to pull info from context
+  ## FIXME reference cobalt1 set
+  my $core = $self->{core};
+  my $website = $core->url;
+  
+  my $context_ref = $core->Servers->{$context} // return 'context failure';
+  my $irc_obj = $context_ref->{Object};
+  return $str unless ref $irc_obj;
+
+  my $ccfg = $core->get_core_cfg;
+  my $cmdchar = $ccfg->{Opts}->{CmdChar};
+  my @users = $irc_obj->channel_list($channel) if $channel;
+  my $random = $users[ rand @users ] if @users;
+
+  my $vars = {
+    '!' => $cmdchar,          ## CmdChar
+    B => $irc_obj->nick_name, ## bot's nick for this context
+    C => $channel,            ## channel
+    H => $irc_obj->nick_long_form($nick), # n!u@h
+    N => $nick,               ## nickname
+    P => $irc_obj->port,      ## remote port
+    Q => $str,                ## question string
+    R => $random,             ## random nickname
+    S => $irc_obj->server,    ## current server
+    t => time,                ## unixtime
+    T => scalar localtime,    ## parsed time
+    V => 'cobalt-'.$core->version,  ## version
+    W => $core->url,          ## website
+  };
+  
+  ##  1~ 2~ .. etc
+  my $x = 0;
+  for my $item (split ' ', $str) {
+    ++$x;
+    $vars->{$x} = $item;
+  }
+
+  ## var replace kinda like rplprintf
+  ## maintains darkbot legacy syntax
+  sub _info3repl {
+    my ($orig, $match, $vars) = @_;
+    return $orig unless defined $vars->{$match};
+    return $vars->{$match}
+  }
+  my $re = qr/((\S)~)/;
+  $str =~ s/$re/_info3repl($1, $2, $vars)/ge;
+  return $str
 }
 
 
