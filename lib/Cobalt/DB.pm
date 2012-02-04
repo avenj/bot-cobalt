91c0dcad94e539edab81b67c727225406b446b07
avenj <avenj@cobaltirc.org>
2012-02-03 23:49:12 -0500

    Cobalt::DB; file locking

--- lib/Cobalt/DB.pm
+++ lib/Cobalt/DB.pm
@@ -1,12 +1,10 @@
 package Cobalt::DB;
-our $VERSION = '0.02';
+our $VERSION = '0.04';
 
 ## ->new(File => $path)
 ##  To use a different lockfile:
 ## ->new(File => $path, LockFile => $lockpath)
 ## Represents a BerkDB
-
-my %h;
 
 use 5.12.1;
 use strict;
@@ -14,7 +12,9 @@ use warnings;
 use Carp;
 
 use DB_File;
-use Fcntl;
+use Fcntl qw/:flock/;
+
+use Cobalt::Serializer;
 
 use File::Spec;
 
@@ -37,51 +37,108 @@ sub new {
                         : $dir . "/.lock.".$dbfile ;
 
   $self->{DatabasePath} = $path;
+
+  $self->{Serializer} = Cobalt::Serializer->new(Format => 'JSON');
   
-  ## FIXME perms arg
-  $self->{Perms} = 0644;
+  $self->{Perms} = $args{Perms} ? $args{Perms} : 0644 ;
 
   return $self
 }
 
-sub _dbopen {
-  my ($self) = shift;
-  ## FIXME lockfile (->fd method?)
+sub dbopen {
+  my $self = shift;
+
   my $path = $self->{DatabasePath};
-  $self->{DB} = tie %h, "DB_File", $path,
+
+  open my $lockfile, '>', $self->{LockFile}
+    or croak "could not create lockfile $self->{LockFile}: $!";
+  flock($lockfile, LOCK_EX) or croak "lock failed: $lockfile: $!";
+  print $lockfile $$;
+  $self->{LockFH} = $lockfile;
+
+  $self->{DB} = tie %{ $self->{Tied} }, "DB_File", $path,
       O_CREAT|O_RDWR, $self->{Perms}, $DB_HASH
       or croak "failed db open: $path: $!"
   ;
+
+  ## null-terminated to be C-compat
+  $self->{DB}->filter_fetch_key(
+    sub { s/\0$// }
+  );
+  $self->{DB}->filter_store_key(
+    sub { $_ .= "\0" }
+  );
+
+  ## Storable is probably faster
+  ## ... but has no backwards compat guarantee
+  $self->{DB}->filter_fetch_value(
+    sub {
+      s/\0$//;
+      $_ = $self->{Serializer}->thaw($_);
+    }
+  );
+  $self->{DB}->filter_store_value(
+    sub {
+      $_ = $self->{Serializer}->freeze($_);
+      $_ .= "\0";
+    }
+  );
+  return 1
 }
 
-sub _dbclose {
+sub get_db {
   my $self = shift;
-  ## FIXME unlock
+  return $self->{DB}
+}
+
+sub dbclose {
+  my $self = shift;
   $self->{DB} = undef;
-  untie %h;
+  untie %{ $self->{Tied} };
+  my $lockfh = $self->{LockFH};
+  flock($lockfh, LOCK_UN) or carp "unlock failed: $!";
+  close $lockfh;
+  delete $self->{LockFH};
+  unlink $self->{LockFile};
 }
 
-sub get_key {
- my ($self, $key) = @_;
- $self->_dbopen;
- my $value = $h{$key} // undef;
- $self->_dbclose;
- return $value
+sub dblock {
+  my $self = shift;
 }
 
-sub set_key {
-  my ($self, $key, $value) = @_;
-  $self->_dbopen;
-  $h{$key} = $value;
-  $self->_dbclose;
+sub dbunlock {
+  my $self = shift;
+  
 }
 
-sub del_key {
+sub keys {
+  my $self = shift;
+  return keys %{ $self->{Tied} }
+}
+
+sub get {
   my ($self, $key) = @_;
-  $self->_dbopen;
-  return undef unless exists $h{$key};
-  delete $h{$key};
+  my $value = $self->{Tied}{$key} // undef;
+  return $value
+}
+
+sub add { put(@_) }
+sub put {
+  my ($self, $key, $value) = @_;
+  $self->{Tied}{$key} = $value;
+  return $value
+}
+
+sub del {
+  my ($self, $key) = @_;
+  return undef unless exists $self->{Tied}{$key};
+  delete $self->{Tied}{$key};
   return 1
 }
 
 1;
+__END__
+
+=pod
+
+=cut
