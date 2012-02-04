71c052760818db89fd96b98d8716a17d7cc39d97
avenj <avenj@cobaltirc.org>
2012-02-04 03:13:35 -0500

    Cobalt::DB; DESTROY method, better checking for DBOPEN condition

--- lib/Cobalt/DB.pm
+++ lib/Cobalt/DB.pm
@@ -1,5 +1,5 @@
 package Cobalt::DB;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 ## ->new(File => $path)
 ##  To use a different lockfile:
@@ -96,16 +96,23 @@ sub dbopen {
       $_ .= "\0";
     }
   );
+  $self->{DBOPEN} = 1;
   return 1
 }
 
 sub get_db {
   my $self = shift;
+  croak "attempted to get_db on unopened db"
+    unless $self->{DBOPEN};
   return $self->{DB}
 }
 
 sub dbclose {
   my $self = shift;
+  unless ($self->{DBOPEN}) {
+    carp "attempted dbclose on unopened db";
+    return
+  }
   $self->{DB} = undef;
   untie %{ $self->{Tied} };
   my $lockfh = $self->{LockFH};
@@ -113,15 +120,25 @@ sub dbclose {
   close $lockfh;
   delete $self->{LockFH};
   unlink $self->{LockFile};
+  $self->{DBOPEN} = 0;
+}
+
+sub DESTROY {
+  my $self = shift;
+  $self->dbclose if $self->{DBOPEN};
 }
 
 sub keys {
   my $self = shift;
+  croak "attempted 'keys' on unopened db"
+    unless $self->{DBOPEN};
   return keys %{ $self->{Tied} }
 }
 
 sub get {
   my ($self, $key) = @_;
+  croak "attempted 'get' on unopened db"
+    unless $self->{DBOPEN};
   my $value = $self->{Tied}{$key} // undef;
   return $value
 }
@@ -129,12 +146,16 @@ sub get {
 sub add { put(@_) }
 sub put {
   my ($self, $key, $value) = @_;
+  croak "attempted 'put' on unopened db"
+    unless $self->{DBOPEN};
   $self->{Tied}{$key} = $value;
   return $value
 }
 
 sub del {
   my ($self, $key) = @_;
+  croak "attempted 'del' on unopened db"
+    unless $self->{DBOPEN};
   return undef unless exists $self->{Tied}{$key};
   delete $self->{Tied}{$key};
   return 1
@@ -144,5 +165,121 @@ sub del {
 __END__
 
 =pod
+
+=head1 NAME
+
+Cobalt::DB - Locking Berkeley DBs with serialization
+
+=head1 SYNOPSIS
+
+  use Cobalt::DB;
+  
+  ## ... perhaps in a Cobalt_register ...
+  my $db_path = $core->var ."/MyDatabase.db";
+  $self->{DB} = Cobalt::DB->new(
+    File => $db_path,
+  );
+  
+  ## do some work:
+  $self->{DB}->dbopen;
+  
+  $self->{DB}->put("SomeKey",
+    { Some => {
+        Deep => { Structure => 1, },
+    }, },
+  );
+  
+  for my $key ($self->{DB}->keys) {
+    my $this_hash = $self->{DB}->get($key);
+  }
+  
+  $self->{DB}->dbclose;
+
+
+=head1 DESCRIPTION
+
+B<Cobalt::DB> provides a simple object-oriented interface to basic 
+L<DB_File> (Berkeley DB 1.x) usage.
+
+BerkDB is a fairly simple key/value store. This module uses JSON to 
+store nested Perl data structures, providing easy database-backed 
+storage for B<Cobalt> plugins.
+
+=head2 Constructor
+
+B<new()> is used to create a new Cobalt::DB object representing your 
+Berkeley DB:
+
+  my $db = Cobalt::DB->new(
+    File => $path_to_db,
+
+   ## Optional arguments:
+    Perms => $octal_mode,
+    # Locking is enabled regardless
+    # but you can change the location:
+    LockFile => $path_to_lockfile,
+  );
+
+=head2 Opening and closing
+
+Database operations should be contained within a dbopen/dbclose:
+
+  ## open, put, close:
+  $db->dbopen;
+  $db->put($key, $data);
+  $db->dbclose;
+  
+  ## open, read, close:
+  $db->dbopen;
+  my $data = $db->get($key);
+  $db->dbclose;
+
+Methods will fail if the DB is not open.
+
+=head2 Locking
+
+By default, a lock file will be created in the same directory as 
+the database file itself.
+
+B<The lock file is cleared on dbclose. Be sure to always dbclose!>
+
+If the Cobalt::DB object is destroyed, it will attempt to dbclose 
+for you.
+
+=head2 Methods
+
+=head3 put
+
+The B<put> method adds an entry to the database:
+
+  $db->put($key, $value);
+
+The value can be any data structure serializable by JSON::XS; that is to 
+say, any shallow or deep data structure NOT including blessed references.
+
+(Yes, Storable is probably faster. JSON is used because it is trivially 
+portable to any language that can interface with BerkDB.)
+
+=head3 get
+
+The B<get> method retrieves a (deserialized) key.
+
+  $db->put($key, { Some => 'hash' } );
+  ## . . . later on . . .
+  my $ref = $db->get($key);
+
+
+=head3 del
+
+The B<del> method removes a key from the database.
+
+  $db->del($key);
+
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+L<http://www.cobaltirc.org>
 
 =cut
