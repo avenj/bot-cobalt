e1357cad0a2eeb8f38b76ea146119333acb41bed
avenj <avenj@cobaltirc.org>
2012-02-10 03:54:17 -0500

    Cobalt::RPL method-autoviv rpl formatter

---
+++ lib/Cobalt/RPL.pm
@@ -0,0 +1,75 @@
+package Cobalt::RPL;
+our $VERSION = '0.01';
+
+## Object returned by $core->rpl_parser
+
+use 5.12.1;
+use strict;
+use warnings;
+
+use Cobalt::Utils qw/rplprintf/;
+
+our $AUTOLOAD;
+
+sub new {
+  my $self = {};
+  bless $self, shift;
+  
+  my %args = @_;
+  
+  my $langset = $args{Lang};
+  unless ($langset && ref $langset eq 'HASH') {
+    warn "Cobalt::RPL needs to be passed a langset via Lang\n";
+    return
+  }
+  
+  $self->{__LANG} = $langset;
+  
+  return $self
+}
+
+sub Format {
+  my ($self, $rpl) = @_;
+  return unless $rpl;
+  return "Missing/undef RPL: $rpl" unless $self->{__LANG}->{$rpl};
+  
+  my $vars;
+  for my $var (@{ $self->{__ADDED} }) {
+    $vars->{$var} = $self->$var;
+  }  
+
+  my $formatted = rplprintf( $self->{__LANG}->{$rpl}, $vars );
+  
+  return $formatted
+}
+
+sub AUTOLOAD {
+  ## magic method creation
+  my $self = shift || return undef;
+  
+  return unless index($AUTOLOAD, 'DESTROY') == -1;
+  
+  my ($method) = $AUTOLOAD =~ s/.*:://;
+
+  my $accessor = sub {
+    my $this_self = shift;
+    
+    return $this_self->{$method} = shift
+      if @_;
+    return $this_self->{$method}
+  };
+  
+  {
+    no strict 'refs';
+    *$AUTOLOAD = $accessor;
+  }
+
+  push(@{ $self->{__ADDED} }, $method);
+  
+  ## put $self back:
+  unshift @_, $self;
+  
+  goto &$AUTOLOAD;
+}
+
+1;
