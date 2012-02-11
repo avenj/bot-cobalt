c7828f80ac535528b60683a5397b94247a025217
avenj <avenj@cobaltirc.org>
2012-02-11 14:36:09 -0500

    comment cleanup in RDB::Database

--- lib/Cobalt/Plugin/RDB/Database.pm
+++ lib/Cobalt/Plugin/RDB/Database.pm
@@ -3,15 +3,13 @@ our $VERSION = '0.31';
 
 ## Frontend to managing RDB-style Cobalt::DB instances
 ##
-## We may have a lot of these.
+## We may have a lot of RDBs.
 ## This plugin tries to make it easy to operate on them discretely
 ## with a minimum of angst in the frontend app.
 ## The frontend interfaces take RDB names; we map them to paths and 
 ## attempt to switch our ->{CURRENT} Cobalt::DB object appropriately.
 ##
-## Error status is expressed via RDB::Constants constant values.
-##
-## If you're trying to program against this interface, I apologize.
+## If you're trying to program against this interface, I apologize. :)
 ## Go read the Cobalt::DB POD and write something better instead.
 ##   (  then send it to me! :)  )
 
