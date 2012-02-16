1f58490ed5ce9999e62ffd0b94f81951fd2dcd57
avenj <avenj@cobaltirc.org>
2012-02-16 02:36:19 -0500

    Plugin::Version; should have been version bumped

--- lib/Cobalt/Plugin/Version.pm
+++ lib/Cobalt/Plugin/Version.pm
@@ -1,5 +1,5 @@
 package Cobalt::Plugin::Version;
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 ## Always declare a package name as the first line.
 ## For example, if your module lives in:
 ##   lib/Cobalt/Plugin/User/MyPlugin.pm
