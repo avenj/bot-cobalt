3dc9f71ef7df831c5b1765f38963df44fa929fcf
avenj <avenj@cobaltirc.org>
2012-03-07 15:47:07 -0500

    Plugin::Extras::Karma; !resetkarma command for superusers
    Allow superusers to use the 'resetkarma' command to clear a karmadb
    entry by default; the level required to use the command can be
    adjusted via an Opts->LevelRequired directive in plugins.conf.
    
    	modified:   lib/Cobalt/Plugin/Extras/Karma.pm

--- lib/Cobalt/Plugin/Extras/Karma.pm
+++ lib/Cobalt/Plugin/Extras/Karma.pm
@@ -1,5 +1,5 @@
 package Cobalt::Plugin::Extras::Karma;
-our $VERSION = '0.12';
+our $VERSION = '0.131';
 
 ## simple karma++/-- tracking
 
@@ -30,6 +30,7 @@ sub Cobalt_register {
     [
       'public_msg',
       'public_cmd_karma',
+      'public_cmd_resetkarma',
     ],
   );
   $core->log->info("Registered");
@@ -78,6 +79,53 @@ sub Bot_public_msg {
   return PLUGIN_EAT_NONE
 }
 
+sub Bot_public_cmd_resetkarma {
+  my ($self, $core) = splice @_, 0, 2;
+  my $context = ${$_[0]};
+  my $msg     = ${$_[1]};
+  my $nick    = $msg->{src_nick};
+  
+  my $usr_lev = $core->auth_level($context, $nick)
+                || return PLUGIN_EAT_ALL;
+  my $pcfg = $core->get_plugin_cfg($self);
+  my $req_lev = $pcfg->{PluginOpts}->{LevelRequired} || 9999;
+  return PLUGIN_EAT_ALL unless $usr_lev >= $req_lev;
+
+  my $channel = $msg->{target};
+  my @message = @{ $msg->{message_array} };
+  my $karma_for = lc(shift @message || return PLUGIN_EAT_ALL);
+
+  unless ( $self->{karmadb}->dbopen ) {
+    $core->log->warn("dbopen failure for karmadb");
+    $core->send_event( 'send_message', $context, $channel, 
+      "Failed to open karmadb",
+    );
+    return PLUGIN_EAT_ALL
+  }
+
+  unless ( $self->{karmadb}->get($karma_for) ) {
+    $core->send_event( 'send_message', $context, $channel,
+      "That user has no karmadb entry.",
+    );
+    $self->{karmadb}->dbclose;
+    return PLUGIN_EAT_ALL
+  }
+  
+  if ( $self->{karmadb}->del($karma_for) ) {
+    $core->send_event( 'send_message', $context, $channel,
+      "Cleared karma for $karma_for",
+    );
+  } else {
+    $core->send_event( 'send_message', $context, $channel,
+      "Failed to clear karma for $karma_for",
+    );
+  }
+
+  $self->{karmadb}->dbclose;  
+  
+  return PLUGIN_EAT_ALL
+}
+
 sub Bot_public_cmd_karma {
   my ($self, $core) = splice @_, 0, 2;
   my $context = ${$_[0]};
@@ -122,10 +170,16 @@ Cobalt::Plugin::Extras::Karma - simple karma bot plugin
 
 =head1 SYNOPSIS
 
+  ## Retrieve karma:
   !karma
   !karma <word>
+
+  ## Add or subtract karma:
   <JoeUser> someone++
   <JoeUser> someone--
+  
+  ## Superusers can clear karma:
+  <JoeUser> !resetkarma someone
 
 =head1 DESCRIPTION
 
@@ -133,6 +187,10 @@ A simple 'karma bot' plugin for Cobalt.
 
 Uses L<Cobalt::DB> for storage, saving to B<karma.db> in the instance's 
 C<var/> directory.
+
+If an B<< Opts->LevelRequired >> directive is specified via plugins.conf, 
+the specified level will be permitted to clear karmadb entries. Defaults to 
+superusers (level 9999).
 
 =head1 AUTHOR
 
