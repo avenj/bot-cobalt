2f9e69f1f25c01f701ed5e15a7781134c4ef3066
avenj <avenj@cobaltirc.org>
2012-04-23 23:57:13 -0400

    Cobalt::Timer basics
    	new file:   lib/Cobalt/Timer.pm

---
+++ lib/Cobalt/Timer.pm
@@ -0,0 +1,70 @@
+package Cobalt::Timer;
+
+use 5.10.1;
+use Carp;
+use Moo;
+use Sub::Quote;
+use MooX::Types::MooseLike::Base qw/:all/;
+
+## my $timer = Cobalt::Core::Item::Timer->new(
+##   core  => $core,
+##   delay => $secs,
+##   event => $event,
+##   args  => $args,
+##   alias => $alias
+## );
+
+has 'core'  => ( is => 'rw', isa => Object, required => 1 );
+
+has 'at'    => ( is => 'rw', isa => Num, default => quote_sub q{0} );
+
+has 'delay' => ( is => 'rw', isa => Num,
+  trigger => sub {
+    my ($self, $value) = @_;
+    $self->at( time() + $value );
+  }, 
+);
+
+has 'event' => ( is => 'rw', isa => Str );
+
+has 'args'  => ( is => 'rw', isa => ArrayRef, 
+  default => quote_sub q{[]},
+);
+
+has 'alias' => ( is => 'rw', isa => Str,
+  default => sub { scalar caller }, 
+);
+
+has 'context' => ( is => 'rw', isa => Str, 
+  default => quote_sub q{'Main'},
+);
+
+has 'text'    => ( is => 'rw', isa => Str );
+has 'target'  => ( is => 'rw', isa => Str );
+
+has 'type'  => ( is => 'rw', isa => Str, lazy => 1, 
+  trigger => sub {
+    my ($self, $value) = @_;
+    given (lc($value)) {
+      when ([qw/msg message privmsg action/]) {
+        my $ev_name = $value eq 'action' ? 
+            'send_action' : 'send_message' ;
+        my @ev_args = ( $self->context, $self->target, $self->text );
+        $self->args( \@ev_args );
+        $self->event( $ev_name );
+      }
+      
+      default { carp "Unknown type $value" }
+
+    }
+  },
+);
+
+sub execute {
+  my ($self) = @_;
+  my $args = $self->args;
+  $self->core->send_event( $self->event, @$args );
+}
+
+
+1;
