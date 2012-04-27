12ba10853879af835e7534f9559217f8da625a0d
avenj <avenj@cobaltirc.org>
2012-04-27 01:25:08 -0400

    Frontend::Utils ask_question()
    	modified:   lib/Bot/Cobalt/Frontend/Utils.pm

--- lib/Bot/Cobalt/Frontend/Utils.pm
+++ lib/Bot/Cobalt/Frontend/Utils.pm
@@ -7,20 +7,53 @@ use Carp;
 
 use base 'Exporter';
 
-our @EXPORT = qw/
+our @EXPORT_OK = qw/
   ask_yesno
+  ask_question
 /;
+
+sub ask_question {
+  my %args = @_;
+  
+  my $question = $args{prompt} || croak "No prompt => specified";
+  my $default  = $args{default};
+  
+  STDOUT->autoflush(1);
+  
+  my $input;
+  
+  my $print_and_grab = sub {
+    print "$question ";
+    if (defined $default) {
+      print "[$default] ";
+    } else {
+      print "> ";
+    }
+    $input = <STDIN>;
+    chomp($input);
+    $input = $default if defined $default and $input eq '';
+    $input
+  };
+  
+  $print_and_grab->();
+  
+  until ($input) {
+    print "No input specified.\n";
+    $print_and_grab->();
+  }
+  
+  return $input
+}
 
 sub ask_yesno {
   my %args = @_;
   
   my $question = $args{prompt} || croak "No prompt => specified";
-  
   my $default  = lc(
     substr($args{default}||'', 0, 1) || croak "No default => specified"
   );
   croak "default should be Y or N"
-    unless $default =~ /^[yn]/;
+    unless $default =~ /^[yn]$/;
 
   my $yn = $default eq 'y' ? 'Y/n' : 'y/N' ;
   
@@ -32,18 +65,18 @@ sub ask_yesno {
     print "$question  [$yn] ";
     $input = <STDIN>;
     chomp($input);
-    $input = $default unless defined $input and $input ne '';
-    $input = lc(substr $input, 0, 1);
+    $input = $default if $input eq '';
+    lc(substr $input, 0, 1);
   };
 
   $print_and_grab->();
    
   until ($input ~~ [qw/y n/]) {
-    print "Invalid input; should be either Y(es) or N(o)\n";
+    print "Invalid input; should be either Y or N\n";
     $print_and_grab->();
   }
 
-  return $input eq 'y' ? 1 : 0
+  return $input eq 'y'
 }
 
 1;
