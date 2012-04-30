a26c51adf4f6981bf6dab142ed8c4e6d74e305fd
avenj <avenj@cobaltirc.org>
2012-04-29 20:54:39 -0400

    Override 'add' in Core::ContextMeta::Auth
    	modified:   lib/Bot/Cobalt/Core/ContextMeta/Auth.pm

--- lib/Bot/Cobalt/Core/ContextMeta/Auth.pm
+++ lib/Bot/Cobalt/Core/ContextMeta/Auth.pm
@@ -6,28 +6,100 @@ use strictures 1;
 use Moo;
 use Carp;
 
-use Cobalt::Common qw/:types/;
+use Bot::Cobalt::Common qw/:types/;
 
 around 'add' => sub {
   my $orig = shift;
   my $self = shift;
   
-  ## FIXME
-}
+  ## auth->add(
+  ##   Context  => $context,
+  ##   Username => $username,
+  ##   Host     => $host,
+  ##   Level    => $level,
+  ##   Flags    => $flags,
+  ##   Alias    => $plugin_alias
+  ## )
+
+  my %args = @_;
+  
+  $args{lc $_} = $args{$_} for keys %args;
+  
+  for my $required (qw/context nickname username host level/) {
+    unless (defined $args{$required}) {
+      carp "add() needs at least a Context, Username, Host, and Level";
+      return
+    }
+  }
+  
+  $args{alias} = scalar caller unless $args{alias};
+  $args{flags} = {}            unless $args{flags};
+  
+  my $meta = {
+    Alias => $args{alias},
+    Username => $args{username},
+    Host  => $args{host},
+    Level => $args{level},
+    Flags => $args{flags},
+  };
+
+  $orig->($self, $context, $nickname, $meta);
+};
 
 sub level {
   my ($self, $context, $nickname) = @_;
+
+  return 0 unless defined $context and defined $nickname;
   
+  return 0 unless exists $self->_list->{$context}
+         and ref $self->_list->{$context}->{$nickname};
+  
+  return $self->_list->{$context}->{$nickname}->{Level} // 0
 }
 
 sub flags {
   my ($self, $context, $nickname) = @_;
-  
+
+  return unless exists $self->_list->{$context}
+         and ref $self->_list->{$context}->{$nickname}
+         and ref $self->_list->{$context}->{$nickname}->{Flags} eq 'HASH';
+
+  return $self->_list->{$context}->{$nickname}->{Flags}
 }
 
 sub username {
   my ($self, $context, $nickname) = @_;
   
+  return unless defined $context and defined $nickname;
+  
+  return unless exists $self->_list->{$context}
+         and ref $self->_list->{$context}->{$nickname};
+
+  return $self->_list->{$context}->{$nickname}->{Username}
 }
+
+sub alias {
+  my ($self, $context, $nickname) = @_;
+  return unless defined $context and defined $nickname;
+  
+  return unless exists $self->_list->{$context}
+         and ref $self->_list->{$context}->{$nickname};
+
+  return $self->_list->{$context}->{$nickname}->{Alias}
+}
+
+sub move {
+  my ($self, $context, $old, $new) = @_;
+  ## User changed nicks, f.ex
+  
+  return unless exists $self->_list->{$context}->{$old};
+  ## FIXME check auth 'ownership' ... ?
+  
+  $self->core->log->debug("Adjusting auth nicks; $old -> $new");
+  
+  $self->_list->{$context}->{$new} =  
+    delete $self->_list->{$context}->{$old};
+}
+
 
 1;
