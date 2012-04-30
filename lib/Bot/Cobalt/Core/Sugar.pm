2543b5e130372cbe17f228a2579c5e71d6907fed
avenj <avenj@cobaltirc.org>
2012-04-30 16:42:59 -0400

    Core::Sugar POD
    	modified:   lib/Bot/Cobalt/Core/Sugar.pm

--- lib/Bot/Cobalt/Core/Sugar.pm
+++ lib/Bot/Cobalt/Core/Sugar.pm
@@ -20,14 +20,6 @@ sub core {
   Bot::Cobalt::Core->instance
 }
 
-sub register {
-  core()->plugin_register( @_ )
-}
-
-sub unregister {
-  core()->plugin_register( @_ )
-}
-
 sub broadcast {
   core()->send_event( @_ )
 }
@@ -36,4 +28,76 @@ sub logger {
   core()->log
 }
 
+sub register {
+  core()->plugin_register( @_ )
+}
+
+sub unregister {
+  core()->plugin_register( @_ )
+}
+
 1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Bot::Cobalt::Core::Sugar - Exported sugar for Bot::Cobalt plugins
+
+=head1 SYNOPSIS
+
+  use Bot::Cobalt;
+
+  ## Call core methods . . .
+  my $u_lev = core->auth->level($context, $nickname);
+  my $p_cfg = core->get_plugin_cfg($self);  
+  
+  # Call plugin_register
+  register( $self, 'SERVER', [ 'public_msg' ] );
+  
+  ## Call send_event
+  broadcast( 'message', $context, $channel, $string );
+  
+  ## Call core->log
+  logger->warn("A warning");
+
+=head1 DESCRIPTION
+
+This module provides the sugar imported when you 'use Bot::Cobalt';
+these are simple functions that wrap L<Bot::Cobalt::Core> methods.
+
+=head2 core
+
+Returns the L<Bot::Cobalt::Core> singleton for the running instance.
+
+=head2 broadcast
+
+  broadcast( $event, @args );
+
+Wraps the B<send_event> method available via L<Bot::Cobalt::Core>; 
+syndicates events to the plugin pipeline.
+
+=head2 logger
+
+  logger->info("Log message");
+
+Wrapper for core->log->$method
+
+=head2 register
+
+  register( $self, 'SERVER', [ @events ]);
+
+Wrapper for core->plugin_register; see L<Bot::Cobalt::Manual::Plugins>
+
+=head2 unregister
+
+Wrapper for core->plugin_unregister
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+L<http://www.cobaltirc.org>
+
+=cut
