983a46ab28b8d6b3d47748ce61a164fb18d09315
avenj <avenj@cobaltirc.org>
2012-05-06 16:39:39 -0400

    Add Extras::Ignore
    	modified:   bin/cobalt2-plugin-installcf
    	modified:   lib/Bot/Cobalt/Plugin/Extras/Ignore.pm

--- lib/Bot/Cobalt/Plugin/Extras/Ignore.pm
+++ lib/Bot/Cobalt/Plugin/Extras/Ignore.pm
@@ -1,14 +1,28 @@
 package Bot::Cobalt::Plugin::Extras::Ignore;
 our $VERSION = '0.200_48';
 
-
 use 5.10.1;
 use strict;
 use warnings;
 
-use Object::Pluggable::Constants qw/ :ALL /;
+use Bot::Cobalt;
+use Bot::Cobalt::Common;
 
+sub Cobalt_register {
+  my ($self, $core) = splice @_, 0, 2;
+  
+  return PLUGIN_EAT_NONE
+}
 
+sub Cobalt_unregister {
+  my ($self, $core) = splice @_, 0, 2;
+  
+  return PLUGIN_EAT_NONE
+}
 
+sub Bot_public_cmd_ignore {
+  ## ignore list / ignore add / ignore del
+  ## glob syntax in `list` ?
+}
 
 1;
