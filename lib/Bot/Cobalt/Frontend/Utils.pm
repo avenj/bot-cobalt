28a3932e1ea049e8169f91136528b247a5ac7b56
avenj <avenj@cobaltirc.org>
2012-05-22 13:41:12 -0400

    Frontend::Utils; use old-style select and $|++
    	modified:   lib/Bot/Cobalt/Frontend/Utils.pm

--- lib/Bot/Cobalt/Frontend/Utils.pm
+++ lib/Bot/Cobalt/Frontend/Utils.pm
@@ -5,7 +5,6 @@ use 5.10.1;
 use strictures 1;
 
 use Carp;
-use IO::Handle;
 
 use base 'Exporter';
 
@@ -33,8 +32,8 @@ sub ask_question {
         delete $args{validate}
         : croak "validate => should be a coderef";
   }
-    
-  STDOUT->autoflush(1);
+  
+  select(STDOUT); $|++;
   
   my $input;
   
@@ -90,8 +89,8 @@ sub ask_yesno {
     unless $default =~ /^[yn]$/;
 
   my $yn = $default eq 'y' ? 'Y/n' : 'y/N' ;
-  
-  STDOUT->autoflush(1);
+
+  select(STDOUT); $|++;
 
   my $input;
 
