9910d851a5b590d487482c15f8ceeff359b6fca2
avenj <avenj@cobaltirc.org>
2012-05-22 11:49:22 -0400

    Plugin::Auth; fix keys() fail on <5.14
    	modified:   lib/Bot/Cobalt/Plugin/Auth.pm

--- lib/Bot/Cobalt/Plugin/Auth.pm
+++ lib/Bot/Cobalt/Plugin/Auth.pm
@@ -106,7 +106,7 @@ sub Cobalt_register {
   my %su = ref $superusers eq 'HASH' ? %{$superusers} : ();
   SERVER: for my $context (keys %su) {
 
-    USER: for my $user (keys $su{$context}) {
+    USER: for my $user (keys %{$su{$context}}) {
       ## Usernames on accesslist automatically get lowercased
       ## per rfc1459 rules, aka CASEMAPPING=rfc1459
       ## (we probably don't even know the server's CASEMAPPING= yet)
