9e71b3bc18cc40f711b5888b3c66c0c08d436b18
avenj <avenj@cobaltirc.org>
2012-06-24 19:46:43 -0400

    Revert "IRC.pm; add IRC::Server objects before issuing spawn()"
    This reverts commit 6e76ec11754ebd82e8b1aeef2b7dd4b7af3710ef.
    
    I thought this would fix a bug seen elsewhere, but the bug turned out to
    be me being stupid.
    
    	modified:   lib/Bot/Cobalt/IRC.pm

--- lib/Bot/Cobalt/IRC.pm
+++ lib/Bot/Cobalt/IRC.pm
@@ -225,19 +225,18 @@ sub Bot_ircplug_connect {
   $spawn_opts{password} = $thiscfg->{ServerPass}
     if defined $thiscfg->{ServerPass};
 
-  my $server_obj = Bot::Cobalt::IRC::Server->new(
-    name => $server,
-    prefer_nick => $nick,
-  );
-
-  $core->Servers->{$context} = $server_obj;
-
   my $irc = POE::Component::IRC::State->spawn(%spawn_opts) 
     or logger->error("IRC component spawn() for $context failed")
     and return PLUGIN_EAT_ALL;
 
+  my $server_obj = Bot::Cobalt::IRC::Server->new(
+    name => $server,
+    irc  => $irc,
+    prefer_nick => $nick,
+  );
+
+  $core->Servers->{$context} = $server_obj;
   $self->ircobjs->{$context} = $irc;
-  $server_obj->irc($irc);
 
   ## Attempt to spin up a session.
   if ( $self->_spawn_for_context($context) ) {
@@ -355,7 +354,6 @@ sub _start {
 
   if (defined $pcfg->{Networks}->{$context}->{NickServPass}) {
     logger->debug("Adding NickServ ID for $context");
-
     $irc->plugin_add('NickServID' =>
       POE::Component::IRC::Plugin::NickServID->new(
         Password => $pcfg->{Networks}->{$context}->{NickServPass},
@@ -587,7 +585,7 @@ sub irc_public {
 
 sub irc_msg {
   my ($self, $heap, $kernel) = @_[OBJECT, HEAP, KERNEL];
-  my ($src, $targets, $txt) = @_[ARG0 .. ARG2];
+  my ($src, $target, $txt) = @_[ARG0 .. ARG2];
 
   my $context = $heap->{Context};
   my $irc     = $self->ircobjs->{$context};
@@ -607,7 +605,7 @@ sub irc_msg {
   my $msg_obj = Bot::Cobalt::IRC::Message->new(
     context => $context,
     src     => $src,
-    targets => $$targets,
+    targets => $target,
     message => $txt,
   );
   
@@ -616,7 +614,7 @@ sub irc_msg {
 
 sub irc_notice {
   my ($self, $heap, $kernel) = @_[OBJECT, HEAP, KERNEL];
-  my ($src, $targets, $txt) = @_[ARG0 .. ARG2];
+  my ($src, $target, $txt) = @_[ARG0 .. ARG2];
 
   my $context = $heap->{Context};
   my $irc     = $self->ircobjs->{$context};
@@ -629,7 +627,7 @@ sub irc_notice {
   my $msg_obj = Bot::Cobalt::IRC::Message->new(
     context => $context,
     src     => $src,
-    targets => $targets,
+    targets => $target,
     message => $txt,
   );
   
@@ -638,7 +636,7 @@ sub irc_notice {
 
 sub irc_ctcp_action {
   my ($self, $heap, $kernel) = @_[OBJECT, HEAP, KERNEL];
-  my ($src, $targets, $txt) = @_[ARG0 .. ARG2];
+  my ($src, $target, $txt) = @_[ARG0 .. ARG2];
 
   my $context = $heap->{Context};
   my $irc     = $self->ircobjs->{$context};
@@ -651,7 +649,7 @@ sub irc_ctcp_action {
   my $msg_obj = Bot::Cobalt::IRC::Message->new(
     context => $context,
     src     => $src,
-    targets => $targets,
+    targets => $target,
     message => $txt,
   );
 
@@ -846,9 +844,6 @@ sub irc_invite {
 sub Bot_rehashed {
   my ($self, $core) = splice @_, 0, 2;
   my $type = ${ $_[0] };
-
-  return PLUGIN_EAT_NONE
-    unless $type eq "channels" or $type eq "core";
   
   logger->info("Rehash received, resetting ajoins");
   $self->_reset_ajoins;
