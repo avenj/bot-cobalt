3313061ce95c1e2677d0234bbefe3c0e0d188ea0
avenj <avenj@cobaltirc.org>
2012-07-04 01:19:06 -0400

    Error.pm; push(), unshift(), expanded tests
    	modified:   lib/Bot/Cobalt/Error.pm
    	modified:   t/02_core/00_error.t

--- lib/Bot/Cobalt/Error.pm
+++ lib/Bot/Cobalt/Error.pm
@@ -17,6 +17,18 @@ sub string {
   join '', map { "$_" } @$self
 }
 
+sub push {
+  my $self = shift;
+  push @$self, @_;
+  return $self->new(@$self)
+}
+
+sub unshift {
+  my $self = shift;
+  unshift @$self, @_;
+  return $self->new(@$self)
+}
+
 sub join {
   my ($self, $delim) = @_;
   $delim //= ' ';
@@ -24,4 +36,92 @@ sub join {
 }
 
 
-1
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Bot::Cobalt::Error - Lightweight error objects
+
+=head1 SYNOPSIS
+
+  package SomePackage;
+  
+  sub some_method {
+    . . .
+    
+    die Bot::Cobalt::Error->new(
+      "Some errors occured:",
+      @errors
+    )->join("\n");
+
+    ## ... same as:
+    die Bot::Cobalt::Error->new(
+      "Some errors occured:\n",
+      join("\n", @errors)
+    );
+  }
+  
+  
+  package CallerPackage;
+  
+  use Try::Tiny;
+  
+  try {
+    SomePackage->some_method();
+  } catch {
+    ## $error isa Bot::Cobalt::Error
+    my $error = $_;
+    
+    ## Stringifies to the error string:
+    warn "$error\n";    
+  };
+
+=head1 DESCRIPTION
+
+A lightweight exception object for L<Bot::Cobalt>.
+
+B<new()> takes a list of messages used to compose an error string.
+
+These objects stringify to the stored errors.
+
+=head2 string
+
+Returns the current error string; this is the same value returned when 
+the object is stringified, such as:
+
+  warn "$error\n";
+
+=head2 join
+
+  $error = $error->join("\n");
+
+Returns a new object whose only element is the result of joining the 
+stored list of errors with the specified expression.
+
+Defaults to joining with a single space. Does not modify the existing 
+object.
+
+=head2 push
+
+  $error = $error->push(@errors);
+
+Appends the specified list to the existing array of errors.
+
+Modifies the existing object and also returns a new object.
+
+=head2 unshift
+
+  $error = $error->unshift(@errors);
+
+Prepends the specified list to the existing array of errors.
+
+Modifies the existing object and also returns a new object.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+=cut
--- t/02_core/00_error.t
+++ t/02_core/00_error.t
@@ -1,4 +1,4 @@
-use Test::More tests => 7;
+use Test::More tests => 13;
 use strict; use warnings;
 
 use Try::Tiny;
@@ -21,3 +21,24 @@ cmp_ok( $obj, 'eq', 'There are someerrors here' );
 
 isa_ok( $obj->join, 'Bot::Cobalt::Error' );
 cmp_ok( $obj->join, 'eq', 'There are some errors here' );
+
+
+$obj = new_ok( 'Bot::Cobalt::Error' );
+
+cmp_ok( $obj->push("Some error"), 'eq', 'Some error' );
+cmp_ok( $obj->unshift("Some header"), 'eq', 'Some headerSome error' );
+cmp_ok( $obj->join, 'eq', 'Some header Some error' );
+
+$obj = new_ok( 'Bot::Cobalt::Error' => [
+    "An error"
+  ],
+);
+
+cmp_ok( $obj = 
+  $obj->new( 
+    "An error",
+    "Second"
+  )->unshift("Unshifted")->push("Third")->join(', '),
+  'eq',
+  "Unshifted, An error, Second, Third"
+);
