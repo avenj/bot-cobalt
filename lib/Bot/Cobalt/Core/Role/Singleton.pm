50f1962f8fec48fa97c8924e4f6798516d5e0c28
avenj <avenj@cobaltirc.org>
2012-07-16 20:55:06 -0400

    Core::Role::Singleton; clear_instance, POD
    	modified:   lib/Bot/Cobalt/Core/Role/Singleton.pm

--- lib/Bot/Cobalt/Core/Role/Singleton.pm
+++ lib/Bot/Cobalt/Core/Role/Singleton.pm
@@ -26,6 +26,16 @@ sub has_instance {
   1
 }
 
+sub clear_instance {
+  my $class = ref $_[0] || $_[0];
+  
+  no strict 'refs';
+  
+  ${$class.'::_singleton'} = undef;
+  
+  1
+}
+
 sub is_instanced {
   require Carp;
   Carp::confess("is_instanced is deprecated; use has_instance")
@@ -33,3 +43,55 @@ sub is_instanced {
 
 1;
 __END__
+
+=pod
+
+=head1 NAME
+
+Bot::Cobalt::Core::Role::Singleton
+
+=head1 SYNOPSIS
+
+  package MySingleton;
+  use Moo;  
+  with 'Bot::Cobalt::Core::Role::Singleton';
+
+=head1 DESCRIPTION
+
+A basic L<Moo::Role> implementing singletons for L<Bot::Cobalt>.
+
+A singleton is a class that can only be instanced once.
+
+Classes can consume this Role in order to gain the following methods:
+
+=head2 instance
+
+The instance method returns the existing singleton if there is one, or 
+calls B<new> to create one if not. Consumers should be instanced by 
+calling B<instance> rather than B<new>:
+
+  ## Bot::Cobalt::Core is a singleton:
+  my $core = Bot::Cobalt::Core->instance(
+    cfg => Bot::Cobalt::Conf->new(etc => $etc),
+    var => $var,
+  );
+
+Arguments are passed to B<new()> unmodified (if we are creating a new 
+singleton).
+
+=head2 has_instance
+
+Returns boolean false if there is currently no instance.
+
+=head2 clear_instance
+
+Clear the singleton instance.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+(Essentially the same as other singleton implementations such as 
+L<Class::Singleton>, L<MooseX::Singleton>, L<MooX::Singleton> etc)
+
+=cut
