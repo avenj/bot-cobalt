bd83c7d781a3714cb3f6e309ff24a3e7f293cbe2
avenj <avenj@cobaltirc.org>
2012-08-12 00:08:24 -0400

    Revert "Plugin::Extras::Money; format result to %.2f"
    This reverts commit d048eac46bce8fd80dc74da5748b1d51c905b956.
    
    	modified:   lib/Bot/Cobalt/Plugin/Extras/Money.pm

--- lib/Bot/Cobalt/Plugin/Extras/Money.pm
+++ lib/Bot/Cobalt/Plugin/Extras/Money.pm
@@ -140,7 +140,7 @@ sub Bot_currencyconv_rate_recv {
   my($rate,$converted);
   if ( $content =~ /<double.*>(.*)<\/double>/i ) {
     $rate = $1||1;
-    $converted = sprintf(".2f", $value * $rate);
+    $converted = $value * $rate ;
   } else {
     broadcast( 'message', $context, $channel,
       "Failed to retrieve currency conversion ($from -> $to)"
